b404520a78c1dc4ebc1551cbb66c6ef61108f72e
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-11-23 21:32:22 +0900

    create online-courses

--- src/components/allcourses/OnlineCourses.js
+++ src/components/allcourses/OnlineCourses.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { online } from "../../localData";
 import Title from "../common/title/Title";
 
 const OnlineCourses = () => {
@@ -7,6 +8,18 @@ const OnlineCourses = () => {
       <section className="online">
         <div className="container">
           <Title subtitle="ジャンル" title="ゆるグラで体験できるジャンル" />
+          <div className="content grid3">
+            {online.map((val) => (
+              <div className="box">
+                <div className="img">
+                  <img src={val.cover} />
+                  <img src={val.hoverCover} className="show" />
+                </div>
+                <h1>{val.courseName}</h1>
+                <span>{val.course}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </>
